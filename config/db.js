18ff7d4a329faeb9fed90d3bab8a91fac56680d5
vishnuacnair <137478225+vishnuacnair@users.noreply.github.com>
2024-01-04 16:02:56 +0530

    mongodb connected atles

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect('mongodb://127.0.0.1:27017/BookMyCourt-FE')
+    const connection = await mongoose.connect('mongodb+srv://infovishnunair:g6g5PdW3AA2j0xmy@cluster0.xidistl.mongodb.net/')
     console.log("mongoDb connected");
   }
   catch (error) {
