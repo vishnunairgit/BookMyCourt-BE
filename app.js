c48198d0f33b9ccfc8baa2e486fbdf91f6d5f596
vishnuacnair <137478225+vishnuacnair@users.noreply.github.com>
2024-01-04 17:25:06 +0530

    db switched to atles, cors orgin updated

--- app.js
+++ app.js
@@ -37,17 +37,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(cors());
+app.use(cors({ 
+  origin:['https://bookmycourt-app-4yrm.onrender.com','http://localhost:3000']
+ }))
 
 
 // cors error resolving
 
-const corsOptions ={
-    origin:['https://bookmycourt-app-4yrm.onrender.com','http://localhost:3000'], 
-    // credentials:true,            //access-control-allow-credentials:true
-    // optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+// const corsOptions ={
+//     origin:['https://bookmycourt-app-4yrm.onrender.com','http://localhost:3000'], 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
 
 
 // const corsOptions = {
