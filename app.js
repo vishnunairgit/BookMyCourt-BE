0d03d8310f6a99ea9e3c7783e83cd5a09d03e740
vishnuacnair <137478225+vishnuacnair@users.noreply.github.com>
2024-01-04 16:52:15 +0530

    db switched to atles, cors orgin updated

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // cors error resolving
 
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:['https://bookmycourt-app-4yrm.onrender.com','http://localhost:3000'], 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
